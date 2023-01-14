5efe6622cd39ef6cf285837dabdc4e2354ee606f
CategorIAN <ianholm9@gmail.com>
2023-01-14 15:39:55 -0700

    Cleaned Up Code.

--- ABCD.py
+++ ABCD.py
@@ -3,18 +3,19 @@ import numpy as np
 from People import People
 import os
 from copy import copy
+from ABCD_Form import ABCD_Form
+from Form import Form
+from itertools import product
 
 
-class ABCD:
-
+class ABCD (Form):
     def __init__(self, df):
-        df = df.rename(self.rename_columns, axis=1)
+        self.F = ABCD_Form()
+        self.directory = os.getcwd() + '\\' + 'ABCD' + '\\'
+        df = df.rename(self.F.column_name_transform, axis=1)
         df = df.fillna("")
-        df = self.setDF(df)
+        df = self.setDF(df, self.F.set_features)
         df = self.removeDuplicates(df)
-        df = df.drop(['Timestamp'], axis=1)
-        df = df.sort_values(by='Name')
-        df.reset_index(drop=True, inplace=True)
         df = self.status(df)
         self.active = df[df['Status'] == 'Active'].reset_index(drop=True)
         self.availability()
@@ -23,8 +24,9 @@ class ABCD:
         self.guest_games()
         self.countMeals()
         self.countAllergies()
-        features = {'Games', 'Game_Types', 'Meals', 'Allergies', 'Platform'}
-        self.df = self.stringDF(df, features)
+        self.guest_food()
+        self.countPlatforms()
+        self.df = self.stringDF(df, self.F.set_features)
 
 
     def status(self, df):
@@ -34,25 +36,16 @@ class ABCD:
         please_remove = '"I would like to be taken off of this gaming list." (You can ask to be put back on this list ' \
                         'whenever you want.)'
         d = {active: 'Active', not_now: "Not_Now", please_remove: 'Please_Remove'}
-        df['Status'] = df['Status'].apply(lambda x: d[x])
+        df['Status'] = df['Status'].map(d)
         return df
 
-    def availability(self):
-        av = {}
-        av['Name'] = self.active['Name']
-        total = {'Name': 'Total'}
-        for day in ['Friday', 'Saturday', 'Sunday']:
-            for h in self.hours:
-                h = h.partition(' to')[0]
-                t = 0
-                a = []
-                for i in range(self.active.shape[0]):
-                        b = int(day in self.active.at[i, h])
-                        a.append(b)
-                        t += b
-                av["{} [{}]".format(day, h)] = a
-                total["{} [{}]".format(day, h)] = t
-        self.av = pd.concat([pd.DataFrame(av), pd.DataFrame(total, index = [self.active.shape[0]])])
+    def availability(self, append = True):
+        x = pd.Series(product(self.F.days, self.F.hours)).map(lambda dh: (dh[0], dh[1].partition(' to')[0]))
+        y = x.map(lambda dh: ("{} [{}]".format(dh[0], dh[1]), self.active[dh[1]].map(lambda s: int(dh[0] in s))))
+        self.av = pd.DataFrame(dict([('Name', self.active['Name'])] + list(y)))
+        if append:
+            total = dict([("Name", "Total")] + [(col, self.av[col].sum()) for col in self.av.columns])
+            self.av = pd.concat([self.av, pd.DataFrame(total, index=[self.active.shape[0]])])
 
     def games(self):
         g = {}
@@ -88,9 +81,6 @@ class ABCD:
     def guest_games(self):
         gg = {}
         gg['Name'] = self.active['Name']
-        prompt = 'Are there games that you own and know how to play that you would enjoy bringing the game for game " \
-                 "events? If so, which games would you enjoy bringing? (You would be responsible for bringing the game " \
-                 "and explaining the rules.)'
         gg['Guest Games'] = self.active['Own']
         gg = pd.DataFrame(gg)
         index = []
@@ -104,7 +94,7 @@ class ABCD:
         meals = {}
         meals['Name'] = self.active['Name']
         total = {'Name': 'Total'}
-        for meal in self.meals:
+        for meal in self.F.meals:
             t = 0
             a = []
             for i in range(self.active.shape[0]):
@@ -119,91 +109,51 @@ class ABCD:
 
 
     def countAllergies(self):
-        self.allergy_df = pd.DataFrame(index = self.active.index, columns=self.allergies)
+        self.allergy_df = pd.DataFrame(index = self.active.index, columns=self.F.allergies)
         extra_allergies = {}
         self.allergy_df.insert(0, 'Name', self.active['Name'])
         for i in range(self.active.shape[0]):
             their_allergies = copy(self.active.at[i, 'Allergies'])
-            self.allergy_df.loc[i, self.allergies] = pd.Series(self.allergies).map(lambda allergy: int(allergy in their_allergies)).values
-            other = their_allergies.difference(set(self.allergies))
+            self.allergy_df.loc[i, self.F.allergies] = pd.Series(self.F.allergies).map(lambda allergy: int(allergy in their_allergies)).values
+            other = their_allergies.difference(set(self.F.allergies))
             if len(other) > 0: extra_allergies[self.active.at[i, 'Name']] = other
         self.allergy_df.to_csv(self.directory + "ABCD_allergies.csv")
         self.extra_allergies = pd.DataFrame(pd.Series(data = extra_allergies, name = 'Allergies'))
         self.extra_allergies = self.stringDF(self.extra_allergies, {'Allergies'}).reset_index(names=['Name'])
         self.extra_allergies.to_csv(self.directory + "ABCD_extra_allergies.csv")
 
-    def setDF(self, df) -> pd.DataFrame:
-        def toSet(string):
-            s = set()
-            for e in string.split(","):
-                if len(e) > 0 and e != "set()":
-                    s.add(e.strip(" {}'"))
-            return s
-        for column in {'Games', 'Game_Types', 'Meals', 'Allergies', 'Platform'}:
-            df[column] = df[column].apply(lambda x: toSet(x))
-        return df
+    def guest_food(self):
+        gf = {}
+        gf['Name'] = self.active['Name']
+        gf['Guest Food'] = self.active['Guest_Food']
+        gf = pd.DataFrame(gf)
+        index = []
+        for i in range(gf.shape[0]):
+            if len(gf.at[i, 'Guest Food']) > 0:
+                index.append(i)
+        gf = gf.filter(items = index, axis=0).reset_index(drop=True)
+        self.gf = pd.DataFrame(gf)
+        self.gf.to_csv(self.directory + "ABCD_guestfood.csv")
 
-    def stringDF(self, df, features) -> pd.DataFrame:
-        def toString(ss):
-            string = ""
-            for s in ss:
-                string = string + s + ", "
-            return string.strip(", ")
-        for column in features:
-                df[column] = df[column].apply(toString)
-        return df
+    def countPlatforms(self):
+        platforms = {}
+        platforms['Name'] = self.active['Name']
+        total = {'Name': 'Total'}
+        for platform in self.F.platforms:
+            t = 0
+            a = []
+            for i in range(self.active.shape[0]):
+                b = int(platform in self.active.at[i, 'Platforms'])
+                a.append(b)
+                t += b
+            platforms[platform] = a
+            total[platform] = t
+        self.F.platforms = pd.concat([pd.DataFrame(platforms), pd.DataFrame(total, index=[self.active.shape[0]])])
+        self.F.platforms.to_csv(self.directory + "ABCD_platforms.csv")
 
-    def removeDuplicates(self, df):
-        emails = set()
-        for i in reversed(range(df.shape[0])):
-            e = df.at[i, 'Email']
-            if e in emails:
-                df = df.drop(i)
-            else:
-                emails.add(e)
-        return df
 
-    time_stamp = 'Timestamp'
-    email = 'Email Address'
-    name = 'What is your name?'
-    status_q = 'You are currently in my tabletop gaming group. What would you like your status to be? ' \
-             '(If you pick the second or third option, you may skip the rest of the questions in this survey.)'
 
-    games_q = 'Every invite you receive for a game event brings you down the queue, making you less likely to be invited ' \
-            'to the next game event. Therefore, it is important I know what games you are interested in playing. ' \
-            'Which of my games are you interested in playing?'
-    game_types_q = 'What types of games do you enjoy playing?'
-    max_hours = 'What is the maximum number of hours you are willing to play a game in one sitting?'
-    commit = 'Would you be willing to a game commitment over multiple days?'
-    own = 'Are there games that you own and know how to play that you would enjoy bringing the game for game events? ' \
-          'If so, which games would you enjoy bringing? (You would be responsible for bringing the game and explaining ' \
-          'the rules.)'
 
-    meals_q = 'Which of my signature meals would you be willing to eat at events?'
-    allergies = 'What are your food allergies?'
-    bring_food = 'What food and/or drinks would you be willing to bring to a gaming event?'
-
-    platform = 'It is efficient to communicate using a group communication platform for invitations and coordination ' \
-               'of details for events. Which of these group communication platforms would you be willing to use?'
-
-    rename_columns = {time_stamp: 'Timestamp', email: 'Email', name: 'Name', status_q: 'Status', games_q: 'Games',
-                      game_types_q: 'Game_Types', max_hours: 'Max_Hours', commit: 'Commit', own: 'Own', meals_q: 'Meals',
-                      allergies: 'Allergies', bring_food: 'Bring_Food', platform: 'Platform'}
-
-    hours = ["11:00 AM to 12:00 PM", "12:00 PM to 1:00 PM", "1:00 PM to 2:00 PM", "2:00 PM to 3:00 PM",
-             "3:00 PM to 4:00 PM", "4:00 PM to 5:00 PM", "5:00 PM to 6:00 PM", "6:00 PM to 7:00 PM"]
-    question = "What times are you possibly available to play games?"
-
-    meals = ['Chicken Cacciatore', 'Halibut in Lemon Wine Sauce', 'Hot Crab Dip', 'Peanut Butter Hummus',
-            'Quinoa Lentil Berry Salad', 'Rosemary Pork and Mushrooms', 'Spaghetti and Classic Marinara Sauce',
-             'Spinach and Artichoke Dip', 'Sweet Potato Casserole']
-
-    allergies = ['Gluten', 'Dairy', 'Peanuts', 'Shellfish']
-
-    for i in range(len(hours)):
-        rename_columns["{} [{}]".format(question, hours[i])] = hours[i].partition(' to')[0]
-
-    directory = os.getcwd() + '\\' + 'ABCD' + '\\'
 
 
 
